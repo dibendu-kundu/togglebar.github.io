1a0e25c498bcba3409c58616d834fd90c20d322e
Dibendu21062000 <72265630+Dibendu21062000@users.noreply.github.com>
2022-07-29 01:20:54 +0530

    Update j5.js

--- j5.js
+++ j5.js
@@ -1,12 +1,43 @@
+// // sliding left right function
+// function scrollleft(){
+//     var left = document.querySelector(".slidebox");
+//     left.scrollBy(250,0);
+// }
+
+// function scrollright(){
+//     var right = document.querySelector(".slidebox");
+//     right.scrollBy(-250,0);
+// }
+
+// // const Bar=document.querySelector(".bar a");
+// // const Order=document.querySelector(".order");
+
+// // Bar.onclick = () => {
+// //     // Bar.classList.toggle("active");
+// //     Order.classList.toggle("active");
+// // };
+
+// // toggle function
+// function myfunc(){
+//     var x =document.getElementById("menu");
+//     if(x.style.display=="block")
+//     {
+//         x.style.display="none";
+//     }
+//     else
+//     {
+//         x.style.display="block";
+//     }
+// }
 // sliding left right function
 function scrollleft(){
     var left = document.querySelector(".slidebox");
-    left.scrollBy(250,0);
+    left.scrollBy(-310,0);
 }
 
 function scrollright(){
     var right = document.querySelector(".slidebox");
-    right.scrollBy(-250,0);
+    right.scrollBy(310,0);
 }
 
 // const Bar=document.querySelector(".bar a");
@@ -17,15 +48,40 @@ function scrollright(){
 //     Order.classList.toggle("active");
 // };
 
+
+
+
 // toggle function
+// function myfunc(){
+//     var x =document.getElementById("menu");
+//     if(x.style.display=="block")
+//     {
+//         x.style.display="none";
+//     }
+//     else
+//     {
+//         x.style.display="block";
+//     }
+// }
+
+
+
 function myfunc(){
-    var x =document.getElementById("menu");
+    var x =document.querySelector(".order");
+    var y =document.querySelector(".cancel");
+    var z =document.querySelector(".bar");
     if(x.style.display=="block")
     {
         x.style.display="none";
+        y.style.display="none";
+        z.style.display="block";
+        
     }
     else
     {
         x.style.display="block";
+        y.style.display="block";
+        z.style.display="none";
     }
 }
+
